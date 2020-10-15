979e97108a856d5af3395740a07ed7a8ca517faf
lexun1060 <getelementbyidkango@gmail.com>
2020-10-15 22:51:59 +0300

    realized onmouse

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -13,7 +13,7 @@ export class Formula extends ExcelComponent {
     toHTML() {
         return ` 
          <div class="info">fx</div>
-        <div class="input" contenteditable spellcheck="false"></div>
+         <div class="input" contenteditable spellcheck="false"></div>
 `
     }
 
