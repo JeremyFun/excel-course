1e694e54c56fe0de947b4a97783ebd62c462d3a0
lexun1060 <getelementbyidkango@gmail.com>
2020-10-20 22:13:00 +0300

    completed style and realized structure table on table.resize and table.function

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,6 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
-import {$} from "@core/dom";
+import {resizeHandler} from "@/components/table/table-resize";
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -17,30 +17,7 @@ export class Table extends ExcelComponent {
 
     onMousedown(event) {
         if (event.target.dataset.resize) {
-            const $resizer = $(event.target)
-            // const $parent = $resizer.$el.closest(`[data-type="resizable"]`)
-            const $parent = $resizer.closest(`[data-type="resizable"]`)
-            const coords = $parent.getCoords()
-            const type = $resizer.data.resize
-
-            const cell = this.$root.findAll(`[data-cell="${$parent.data.col}"]`)
-
-            document.onmousemove = e => {
-                if (type === 'col') {
-                    const delta = e.clientX - coords.right
-                    const value = coords.width + delta
-                    $parent.css({width: value + 'px'})
-                    cell.forEach(cell => cell.style.width = value + 'px')
-                } else {
-                    const delta = e.clientY - coords.bottom
-                    const value = coords.height + delta
-                    $parent.css({height: value + 'px'})
-                }
-            }
-            document.onmouseup = () => {
-                document.onmousemove = null
-                document.onmouseup = null
-            }
+            resizeHandler(this.$root, event)
         }
     }
 
