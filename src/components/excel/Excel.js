4928d4a7c95d0c11096ef71783e3d082052d7b5e
Kango <getelementbyidkango@gmail.com>
2020-11-01 20:03:01 +0200

    without mistake

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -16,7 +16,8 @@ export class Excel {
 
         this.components = this.components.map(Component => {
             const $el = $.create('div', Component.className)
-            const component = new Component($el, componentOptions) // // DEBUG
+            const component = new Component($el, componentOptions)
+            // DEBUG
             // if (component.name) {
             //   window['c' + component.name] = component
             // }
